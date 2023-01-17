9cf0bc4cfcb58b81968c0f4e3f121dd90b12f804
adoums <66362633+Adoums@users.noreply.github.com>
2023-01-17 19:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {request} = require("https")
-const { exec } = require("child_process");
+const {exec} = require("child_process")
+const {appendFileSync} = require("fs")
 const token = process.env.token
-let seq = null, session_id, heartBeat, gatewayUrl = "wss://gateway.discord.gg/?v=10&encoding=json"
+let seq = null, session_id, interval, gatewayUrl = "wss://gateway.discord.gg/?v=10&encoding=json", resume = true
 function got(method, endpoint, data, headers = {}) {
     return new Promise(resolve => request({
         method,
@@ -17,15 +18,15 @@ function got(method, endpoint, data, headers = {}) {
         res.on("data", chunk => body += chunk)
         res.on("end", () => {
           if(body.startsWith("error")) {
-            require("fs").appendFileSync("logs.txt", `Error (${new Date().toUTCString()})\n`)
+            appendFileSync("logs.txt", `Error (${new Date().toUTCString()})\n`)
             exec("node index")
             process.kill(1)
           }
+          let result = {status: res.statusCode}
             try {
-                resolve({body: JSON.parse(body), status: res.statusCode, headers: res.headers})
-            } catch(e) {
-                resolve({body: null, status: res.statusCode, headers: res.headers})
-            }
+                Object.assign(result, JSON.parse(body))
+            } catch(e) {}
+          resolve(result)
         })
     }).end(data))
 }
@@ -35,43 +36,54 @@ function got(method, endpoint, data, headers = {}) {
         if(bool) return ws.send(`{"op":6,"d":{"token":"${token}","seq":${seq},"session_id":"${session_id}"}}`)
         ws.send(`{"op":2,"d":{"token":"${token}","properties":{"os":"linux"},"intents":4096,"presence":{"status":"online","afk":false,"activities":[{"name":"/propose => DM","type":1,"url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}]}}}`)
     })
+    function close(bool) {
+      resume = bool
+      ws.close(1000)
+    }
     ws.on("message", async m => {
         m = JSON.parse(m)
         seq = m.s || seq
         if(m.op == 1) return ws.send(`{"op":1,"d":${seq}}`)
-        if(m.op == 7) return ws.close()
-        if(m.op == 9) return console.error("Invalid Token")
-        if(m.op == 10) return heartBeat = setInterval(() => ws.send(`{"op":1,"d":${seq}}`), m.d.heartbeat_interval)
+        if(m.op == 7) return close(true)
+        if(m.op == 9 && m.d) return close(false)
+        else if(m.op == 9) return close(true)
+        if(m.op == 10) return interval = setInterval(() => ws.send(`{"op":1,"d":${seq}}`), m.d.heartbeat_interval)
         switch(m.t) {
             case "READY":
                 session_id = m.d.session_id
-                gatewayUrl = m.d.resume_gateway_url
+                gatewayUrl = `${m.d.resume_gateway_url}/?v=10&encoding=json`
                 break
             case "MESSAGE_CREATE":
                 if(m.d.author.id != "1030514575449272381") await got("POST", `/channels/${m.d.channel_id}/messages`, `{"content":"Voici mes commandes :\\n\\n\`/propose\` : Proposer une idée ou annoncer quelque chose à la classe."}`)
                 break
             case "INTERACTION_CREATE":
-                if(m.d.data.name == "propose") return got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":4,"data":{"content":"Cliquez sur le bouton ci-dessous afin d'envoyer un message publiquement dans <#1030525904008523776>","flags":64,"components":[{"type":1,"components":[{"type":2,"custom_id":"type","style":1,"label":"Envoyer un message"}]}]}}`)
+                if(m.d.data.name == "propose") return got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":4,"data":{"content":"Voici votre message : \`\`\`${m.d.data.options[0].value.replace(/["`]/gm, '\\"').replace(/\n/gm, "\\n")}\`\`\`","flags":64,"components":[{"type":1,"components":[{"type":2,"custom_id":"anonyme","style":1,"label":"Envoyer anonymement","emoji":{"name":"🎭"}},{"type":2,"custom_id":"edit","style":1,"label":"Modifier","emoji":{"name":"📝"}}]}]}}`)
                 switch(m.d.data.custom_id) {
-                    case "type":
-                        await got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":9,"data":{"title":"Saisissez le message a envoyer","custom_id":"message","components":[{"type":1,"components":[{"type":4,"style":2,"label":"Message","custom_id":"message"}]}]}}`)
-                        break
                     case "message":
-                        await got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":7,"data":{"content":"Voici votre message : \`\`\`${m.d.data.components[0].components[0].value.replace(/["`]/gm, '\\"').replace(/\n/gm, "\\n")}\`\`\`\\nVeuillez selectionner votre mode de publication :","flags":64,"components":[{"type":1,"components":[{"type":2,"custom_id":"anonyme","style":1,"label":"Envoyer anonymement","emoji":{"name":"🎭"}},{"type":2,"custom_id":"edit","style":1,"label":"Modifier","emoji":{"name":"📝"}}]}]}}`)
+                        await got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":7,"data":{"content":"Voici votre message : \`\`\`${m.d.data.components[0].components[0].value.replace(/["`]/gm, '\\"').replace(/\n/gm, "\\n")}\`\`\`","flags":64,"components":[{"type":1,"components":[{"type":2,"custom_id":"anonyme","style":1,"label":"Envoyer anonymement","emoji":{"name":"🎭"}},{"type":2,"custom_id":"edit","style":1,"label":"Modifier","emoji":{"name":"📝"}}]}]}}`)
                         break
                     case "edit":
                         await got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":9,"data":{"title":"Saisissez le message a envoyer","custom_id":"message","components":[{"type":1,"components":[{"type":4,"style":2,"label":"Message","custom_id":"message"}]}]}}`)
                         break
                     case "anonyme":
                         await got("POST", `/interactions/${m.d.id}/${m.d.token}/callback`, `{"type":7,"data":{"content":"Votre message a bien été envoyé **en anonyme** !","components":[],"flags":64}}`)
-                        await got("POST", `/webhooks/1030580687817932840/JKHISvMJ5nkwuzvruLTvgn1EBWcr4ffxwYnIPPVfGci50CI4WM8o3v-WbHzwCMFN-ChD`, `{"username":"Anonyme","content":"${m.d.message.content.match(/(?<=```)[^`]+(?=```)/gm)[0].replace(/["`]/gm, '\\"').replace(/\n/gm, "\\n")}"}`)
+                        let message = await got("POST", `/webhooks/1030580687817932840/JKHISvMJ5nkwuzvruLTvgn1EBWcr4ffxwYnIPPVfGci50CI4WM8o3v-WbHzwCMFN-ChD?wait=true`, `{"username":"Anonyme","content":"${m.d.message.content.match(/(?<=```)[^`]+(?=```)/gm)[0].replace(/["`]/gm, '\\"').replace(/\n/gm, "\\n")}"}`)
+                        await got("PUT", `/channels/${message.channel_id}/messages/${message.id}/reactions/yes:1047189209057873920/@me`)
+                        setTimeout(() => got("PUT", `/channels/${message.channel_id}/messages/${message.id}/reactions/no:1047189425110650950/@me`), 500)
                         break
                 }
                 break
         }
     })
-    ws.on("close", () => {connect(true); clearInterval(heartBeat)})
+    ws.on("close", (code) => {
+      clearInterval(interval)
+      if(code >= 4000 || !resume) {
+        appendFileSync("logs.txt", `${code}\n`)
+        return connect(false)
+      }
+      connect(true)
+    })
 })()
-require("http").createServer((req, res) => res.end('holà')).listen(process.env.port || 9001)
-process.on("uncaughtException", async(error) => require("fs").appendFileSync("logs.txt", await got("POST", "/webhooks/1028407149048643615/Hr51KiTG8obi6cIG2k8ofXQMCgmQvxKZ_3raWaNTHvo2OtyMnS1XAGm2mtI_RVdZ8dzd", `{"content":"${error.toString().replace(/"/gm,'\\"')}"}`), {authorization: null}))
-process.on("unhandledRejection", async(error) => require("fs").appendFileSync("logs.txt", await got("POST", "/webhooks/1028407149048643615/Hr51KiTG8obi6cIG2k8ofXQMCgmQvxKZ_3raWaNTHvo2OtyMnS1XAGm2mtI_RVdZ8dzd", `{"content":"${error.toString().replace(/"/gm,'\\"')}"}`), {authorization: null}))
+require("http").createServer((req, res) => res.end()).listen(80)
+process.on("unhandledRejection", async(error) => appendFileSync("logs.txt", error.toString()))
+process.on("uncaughtException", async(error) => appendFileSync("logs.txt", error.toString()))
